97bc35ad22a7361ae208c2d93eec2d05c8b78dee
Petrut <ivanoiu.petrut27@yahoo.com>
2023-11-18 23:33:37 +0200

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <button class="btn btn-accent btn-outline">Three</button>
+    <div>
+      <nav>
+        <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
