c39529d99348786b8345bdef89aa819315a245a4
Petrut <ivanoiu.petrut27@yahoo.com>
2023-12-10 22:02:29 +0200

    feat: add login template

---
+++ src/components/login/LoginItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="form-control mb-4">
+    <label class="label" for="input1"><span class="label-text">Email</span></label>
+    <input
+      type="email"
+      placeholder="Email Address"
+      class="input input-bordered input-primary"
+      required
+      id="input1"
+    />
+  </div>
+  <div class="form-control">
+    <label class="label" for="input2"><span class="label-text">Password</span></label>
+    <input
+      type="password"
+      placeholder="Password"
+      class="input input-bordered input-primary"
+      required
+      for="input2"
+    />
+    <label class="label"
+      ><button class="label-text-alt link link-hover">Forgot password?</button></label
+    >
+  </div>
+  <div class="form-control mt-6"><button class="btn btn-primary">Login</button></div>
+  <span class="label-text-alt link link-hover text-sm"
+    >Need account? <strong>Register</strong></span
+  >
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import LoginItem from '@/components/login/LoginItem.vue'
+</script>
+
+<template>
+  <main class="h-screen grid place-items-center">
+    <div class="md:w-96">
+      <h1 class="text-4xl font-extrabold text-center mb-8">Welcome back!</h1>
+      <LoginItem />
+    </div>
+  </main>
+</template>
