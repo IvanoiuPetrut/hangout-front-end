202fced50a900110d997df27ec5bdc29ef06aa1d
Petrut <ivanoiu.petrut27@yahoo.com>
2024-02-28 17:21:30 +0200

    wip: update user details

--- src/services/user/userInteractor.ts
+++ src/services/user/userInteractor.ts
@@ -1,10 +1,45 @@
 import type { UserDetails } from "@/types/types.ts";
+import { ref } from "vue";
 
 import { backendInstanceForInteractor } from "@/services/backendClient";
+
+function useAsyncRequest<T>(request: () => Promise<T>) {
+  const data = ref<any>(null);
+  const error = ref<Error | null>(null);
+  const loading = ref(false);
+
+  const execute = async () => {
+    loading.value = true;
+    try {
+      data.value = await request();
+    } catch (e: any) {
+      console.error(e);
+      error.value = e;
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return {
+    data,
+    error,
+    loading,
+    execute
+  };
+}
 
 async function getUserDetails(): Promise<UserDetails> {
   const userDetails = await backendInstanceForInteractor.get("/user/details");
   return userDetails.data;
 }
 
-export { getUserDetails };
+async function updateUserDetails(userName: string, password: string): Promise<UserDetails> {
+  const body = {
+    username: userName
+  };
+  console.log("body", body);
+  const userDetails = await backendInstanceForInteractor.patch("/user/details", body);
+  return userDetails.data;
+}
+
+export { getUserDetails, updateUserDetails, useAsyncRequest };
--- src/views/UserSettingsView.vue
+++ src/views/UserSettingsView.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { useUserStore } from "@/stores/user";
+import { updateUserDetails, useAsyncRequest } from "@/services/user/userInteractor";
+
+const userName = ref("");
+const password = ref("");
+const photo = ref("");
+
+const { data, error, loading, execute } = useAsyncRequest(() =>
+  updateUserDetails(userName.value, password.value)
+);
+
+function handleSaveSettings(): void {
+  execute();
+}
 </script>
 
 <template>
+  <p>data:{{ data }}</p>
+  <p>error:{{ error }}</p>
+  <p>loading:{{ loading }}</p>
   <div class="flex flex-col gap-8 items-center mt-8 px-4 sm:mt-32">
     <div
       class="flex flex-col sm:flex-row gap-8 sm:gap-24 w-fit p-4 border-2 border-neutral rounded-lg mx-auto"
@@ -14,6 +31,7 @@ import { useUserStore } from "@/stores/user";
               <span class="label-text font-bold uppercase">Dispaly name</span>
             </div>
             <input
+              v-model="userName"
               type="text"
               :placeholder="useUserStore().userName || 'Enter your name'"
               class="input input-bordered input-md w-full max-w-xs"
@@ -26,6 +44,7 @@ import { useUserStore } from "@/stores/user";
               <span class="label-text">Password</span>
             </div>
             <input
+              v-model="password"
               type="password"
               placeholder="••••••••••••"
               class="input input-bordered input-md w-full max-w-xs"
@@ -39,6 +58,6 @@ import { useUserStore } from "@/stores/user";
         <input type="file" class="file-input file-input-bordered file-input-xs w-full max-w-xs" />
       </div>
     </div>
-    <button class="btn btn-primary btn-wide">Save settings</button>
+    <button @click="handleSaveSettings" class="btn btn-primary btn-wide">Save settings</button>
   </div>
 </template>
