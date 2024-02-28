cee86856868976c4b678123d31fca9896b3731ed
Petrut <ivanoiu.petrut27@yahoo.com>
2024-02-28 16:17:21 +0200

    feat: add template for adding friends

--- src/components/friends/AddFriend.vue
+++ src/components/friends/AddFriend.vue
@@ -1,12 +1,65 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const userName = ref("");
+</script>
+
 <template>
-  <button class="btn btn-sm" onclick="my_modal_5.showModal()">Add friend</button>
+  <button class="btn btn-sm btn-primary mx-4" onclick="my_modal_5.showModal()">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+      />
+    </svg>
+    Add friend
+  </button>
   <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
     <div class="modal-box">
-      <h3 class="font-bold text-lg">Hello!</h3>
-      <p class="py-4">Press ESC key or click the button below to close</p>
+      <h3 class="font-bold text-lg">Enter the name of the friend you want to add</h3>
+      <label class="input input-bordered flex items-center gap-2 mt-4">
+        <input
+          v-model="userName"
+          type="search"
+          name="search"
+          placeholder="User name..."
+          autocomplete="off"
+          spellcheck="false"
+          class="grow input-sm bg-base-100"
+        />
+
+        <button class="btn btn-neutral btn-sm">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          Search
+        </button>
+      </label>
+      <ul>
+        <li class="flex items-center gap-2 mt-4">
+          <p class="font-bold">Friend name</p>
+          <button class="btn btn-sm btn-primary">Add</button>
+        </li>
+      </ul>
       <div class="modal-action">
         <form method="dialog">
-          <!-- if there is a button in form, it will close the modal -->
           <button class="btn">Close</button>
         </form>
       </div>
--- src/views/FriendsView.vue
+++ src/views/FriendsView.vue
@@ -128,24 +128,6 @@ function toggleFriendsMenuVisibility(): void {
         class="p-2 overflow-y-auto"
       />
       <AddFriend />
-      <button class="btn btn-sm btn-primary mt-auto mx-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
-          />
-        </svg>
-
-        Add friend
-      </button>
       <button @click="toggleFriendsMenuVisibility" class="btn btn-sm btn-neutral mt-auto mb-2 mx-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -188,7 +170,11 @@ function toggleFriendsMenuVisibility(): void {
         <h3 v-if="selectedFriend" class="mx-auto text-center text-xl px-4 text-accent font-bold">
           {{ selectedFriend.name }}
         </h3>
-        <h3 v-else class="flex gap-2 items-center mx-auto text-center text-xl px-4 font-bold">
+        <h3
+          v-else
+          class="flex gap-2 items-center mx-auto text-center text-xl px-4 font-bold"
+          :class="{ 'ml-24': isFriendsMenuVisible }"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
