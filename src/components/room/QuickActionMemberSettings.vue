133978a0629355e387f97f9cab08316798af141e
Petrut <leonard.lacraru@ulbsibu.ro>
2024-04-10 16:32:11 +0300

    feat: add responsive design for room members

--- src/components/room/QuickActionMemberSettings.vue
+++ src/components/room/QuickActionMemberSettings.vue
@@ -6,15 +6,23 @@ const isMenuVisible = ref(false);
 function toggleMenuVisibility() {
   isMenuVisible.value = !isMenuVisible.value;
 }
+
+function handleButtonClick() {
+  console.log("do stuff");
+  isMenuVisible.value = false;
+}
 </script>
 
 <template>
   <div class="relative">
-    <button @click="toggleMenuVisibility" class="btn btn-neutral btn-sm">Click me</button>
-    <div v-if="isMenuVisible" class="join join-vertical absolute top-10 left-1 z-50">
-      <button class="btn join-item">Button</button>
-      <button class="btn join-item">Button</button>
-      <button class="btn join-item">Button</button>
+    <button @click="toggleMenuVisibility" class="btn btn-neutral btn-sm">Options</button>
+    <div
+      v-if="isMenuVisible"
+      class="join join-vertical w-36 absolute top-10 -left-16 md:-left-8 z-50 shadow-md border border-neutral"
+    >
+      <button @click="handleButtonClick" class="btn join-item">Make moderator</button>
+      <button @click="handleButtonClick" class="btn join-item">Add friend</button>
+      <button @click="handleButtonClick" class="btn join-item">Kick</button>
     </div>
   </div>
 </template>
--- src/components/room/RoomMembers.vue
+++ src/components/room/RoomMembers.vue
@@ -4,20 +4,48 @@ import QuickActionMemberSettings from "@components/room/QuickActionMemberSetting
 
 <template>
   <ul class="flex flex-col gap-4">
-    <li class="flex gap-12 items-center">
-      <div class="flex gap-4 items-center">
-        <div class="mask mask-circle w-12 h-12">
+    <li class="flex gap-2 md:gap-12 items-center justify-between md:justify-normal">
+      <div class="flex gap-2 items-center md:gap-4">
+        <div class="mask mask-circle w-8 h-8 md:w-12 md:h-12">
           <img
             src="https://www.gravatar.com/avatar/3b3be63a4c2asdas01asdasda02?d=identicon"
             alt="Avatar Tailwind CSS Component"
           />
         </div>
-        <p class="font-bold">User name</p>
+        <p class="font-bold text-sm md:text-lg">User name</p>
+      </div>
+      <span class="badge badge-primary badge-sm">Owner</span>
+      <QuickActionMemberSettings />
+    </li>
+
+    <li class="flex gap-2 md:gap-12 items-center justify-between md:justify-normal">
+      <div class="flex gap-2 items-center md:gap-4">
+        <div class="mask mask-circle w-8 h-8 md:w-12 md:h-12">
+          <img
+            src="https://www.gravatar.com/avatar/3b3be63a4c2asdas01asdasda02?d=identicon"
+            alt="Avatar Tailwind CSS Component"
+          />
+        </div>
+        <p class="font-bold text-sm md:text-lg">User name</p>
       </div>
       <span class="badge badge-neutral badge-sm">Member</span>
       <QuickActionMemberSettings />
     </li>
-    <li class="flex gap-12 items-center">
+
+    <li class="flex gap-2 md:gap-12 items-center justify-between md:justify-normal">
+      <div class="flex gap-2 items-center md:gap-4">
+        <div class="mask mask-circle w-8 h-8 md:w-12 md:h-12">
+          <img
+            src="https://www.gravatar.com/avatar/3b3be63a4c2asdas01asdasda02?d=identicon"
+            alt="Avatar Tailwind CSS Component"
+          />
+        </div>
+        <p class="font-bold text-sm md:text-lg">User name</p>
+      </div>
+      <span class="badge badge-secondary badge-sm">Moderator</span>
+      <QuickActionMemberSettings />
+    </li>
+    <!-- <li class="flex gap-2 items-center">
       <div class="flex gap-4 items-center">
         <div class="mask mask-circle w-12 h-12">
           <img
@@ -42,6 +70,6 @@ import QuickActionMemberSettings from "@components/room/QuickActionMemberSetting
       </div>
       <span class="badge badge-secondary badge-sm">Moderator</span>
       <QuickActionMemberSettings />
-    </li>
+    </li> -->
   </ul>
 </template>
