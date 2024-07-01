4ba94704ee12e0aa09c82c301d059e066cddc295
Petrut <ivanoiu.petrut27@gmail.com>
2024-07-01 17:09:07 +0300

    feat: add logout url in left menu

--- src/components/navigation/LeftMenu.vue
+++ src/components/navigation/LeftMenu.vue
@@ -6,6 +6,8 @@ const emit = defineEmits<{
   (e: "toggleMenuVisibility"): void;
 }>();
 
+const logoutUrl = import.meta.env.VITE_COGNITO_LOGOUT_URL as string;
+
 function handleToggleMenuVisibility(): void {
   const smallScreenWidth = 640;
   const smallScreen = window.innerWidth < smallScreenWidth;
@@ -66,9 +68,25 @@ function handleToggleMenuVisibility(): void {
         Join a room
       </RouterLink>
     </li>
-    <li>
-      <JoinedRooms />
+    <li><JoinedRooms /></li>
+    <li class="mt-auto">
+      <a :href="logoutUrl">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-5 h-5"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+
+        Logout</a
+      >
     </li>
-    <BaseAvatar class="mt-auto" />
+    <BaseAvatar />
   </ul>
 </template>
